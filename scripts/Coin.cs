9e144051286dbca5af3823fecfcc7758d7ebe031
frognar <sebastian.przyszlak@gmail.com>
2025-01-27 22:02:52 +0100

    Add bg music and pickup SFX

--- scripts/Coin.cs
+++ scripts/Coin.cs
@@ -5,15 +5,17 @@ namespace FirstGame;
 public partial class Coin : Area2D
 {
 	private GameManager _gameManager;
+	private AnimationPlayer _animationPlayer;
 
 	public override void _Ready()
 	{
 		_gameManager = GetNode<GameManager>("%GameManager");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	private void _on_body_entered(Node2D body)
 	{
 		_gameManager.AddPoint();
-		QueueFree();
+		_animationPlayer.Play("pickup");
 	}
 }
